f1ef3fa29b7948e05d6ecfdc2f33642541056e51
Shashikant Sonawane <codepad.shashi@gmail.com>
2011-03-16 17:36:41 +0530

    [IMP]:Done with URL Decoupling

--- urls.py
+++ urls.py
@@ -4,24 +4,25 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('dblog.views',
+urlpatterns = patterns('',
     # Example:
-    (r'^blog/$', 'index'),
-    (r'^blog/(?P<blog_id>\d+)/$', 'readblog'),
-    (r'^blog/(?P<blog_id>\d+)/comment/$', 'comment'),
-    (r'^blog/(?P<blog_id>\d+)/comment/add/$', 'addComment'),
-    (r'^blog/(?P<blog_id>\d+)/postComment/$', 'postComment'),
-    (r'^blog/writeBlog/$', 'writeBlog'),
-    (r'^blog/postEntry/$', 'postEntry'),
+    #(r'^blog/$', 'index'),
+    #(r'^blog/(?P<blog_id>\d+)/$', 'readblog'),
+    #(r'^blog/(?P<blog_id>\d+)/comment/$', 'comment'),
+    #(r'^blog/(?P<blog_id>\d+)/comment/add/$', 'addComment'),
+    #(r'^blog/(?P<blog_id>\d+)/postComment/$', 'postComment'),
+    #(r'^blog/writeBlog/$', 'writeBlog'),
+    #(r'^blog/postEntry/$', 'postEntry'),
 
     # (r'^simpleblog/$', 'simpleblog.views.index'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-)
-
-urlpatterns += patterns('',
+	(r'^blog/', include('dblog.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+#urlpatterns += patterns('',
+#    (r'^admin/', include(admin.site.urls)),
+#)
